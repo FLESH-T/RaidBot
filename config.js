8f3779b1a87a1ca51a3aec0ead9a8c0376d5acee
FLESH-T <88311238+FLESH-T@users.noreply.github.com>
2021-08-02 13:45:56 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const TOKEN = "046bfbb20a15e64d3d3e3b30f44f391ee80c16c07f1b338e9062739cd16cce1d618aa4d79fb1642d8eabd"; // Токен группы, в которой будет стоять бот
+const GROUP_ID = 206233386; // Айди группы, в которой будет стоять бот
+const BTN_TEXT = ["vto.pe", "vkbot.ru", "vkbot.ru"];
+const TIME = 300;
+const CHAT_SPAM = [".Вас заскамили, ССОРИ))"];
+const HELLO_TEXT = `Bot by FLASH.
+`;
+
+module.exports = {
+    TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
+};
